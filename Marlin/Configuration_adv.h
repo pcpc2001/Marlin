a0904d0e8002049b5a45cecb4932dc1881009dbc
Paul Chen <pcpc2001@hotmail.com>
2020-01-18 15:15:14 -0500

    Changed Y_Current to 840

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1897,7 +1897,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       760
+    #define Y_CURRENT       840
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
