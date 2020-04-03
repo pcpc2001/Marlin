fb5f66653ddfff50cef77d7169450f605618da50
Paul Chen <pcpc2001@hotmail.com>
2020-04-02 22:08:37 -0400

    Invert E direction and Z current.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2068,7 +2068,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       760
+    #define Z_CURRENT       900
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
