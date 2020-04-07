89e8575b26937e1cd40c9a9b2291593f7bdb6437
Paul Chen <pcpc2001@hotmail.com>
2020-04-06 22:58:09 -0400

    Inverted both motors (Y and Z) that are using TMC5160.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1107,8 +1107,8 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
-#define INVERT_Y_DIR true
-#define INVERT_Z_DIR true
+#define INVERT_Y_DIR false // [PC] TMC5160 Seems to invert directions
+#define INVERT_Z_DIR false // [PC] TMC5160 Seems to invert directions
 
 // @section extruder
 
